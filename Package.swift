d62c8c872ef81e790446b64bb59d0ba9bb954219
Tim Schmelter <schmelte@amazon.com>
2020-11-18 08:52:39 -0800

    chore: Build out SPM-based dev workflow

--- Package.swift
+++ Package.swift
@@ -1,10 +1,13 @@
 // swift-tools-version:5.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "AppSyncRealTimeClient",
+    platforms: [
+        .iOS(.v9),
+        .macOS(.v10_15)
+    ],
     products: [
         .library(
             name: "AppSyncRealTimeClient",
--- build-support/Package.swift
+++ build-support/Package.swift
@@ -1,14 +1,22 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.3
 
 import PackageDescription
 
 let package = Package(
     name: "build-support",
+    platforms: [
+        .iOS(.v9),
+        .macOS(.v10_15)
+    ],
     dependencies: [
         .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.47.3"),
         .package(url: "https://github.com/realm/SwiftLint.git", from: "0.41.0")
     ],
     targets: [
-        .target(name: "build-support", path: "")
+        .target(
+          name: "build-support",
+          path: "",
+          exclude: ["setup_private_specs.sh"]
+        )
     ]
 )
